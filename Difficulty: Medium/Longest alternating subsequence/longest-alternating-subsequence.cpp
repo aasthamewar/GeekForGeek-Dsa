df790b969dc12347666b6a59ce1827257a72e0f0
Aastha_am <aastha.mewar2801@gmail.com>
2026-05-31 19:14:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest alternating subsequence/longest-alternating-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int alternatingMaxLength(vector<int>& arr) {
+        // Code here
+        int up=1;
+        int down=1;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]>arr[i-1]){
+                up=down+1;
+            }
+            else if(arr[i]<arr[i-1]){
+                down=up+1;
+            }
+        }
+        return max(up,down);
+    }
+};
