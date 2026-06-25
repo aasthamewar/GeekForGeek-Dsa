ad761e1e15194ec5a50750c9e2fc656526bef85e
Aastha_am <aastha.mewar2801@gmail.com>
2026-06-25 13:01:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,23 +1,25 @@
 class Solution {
   public:
-  void dfsans(int node,vector<vector<int>>&adj, vector<int>&vis,vector<int>&res){
-      vis[node]=1;
-      res.push_back(node);
-      
-      for(int neighbor:adj[node]){
-          if(!vis[neighbor]){
-              dfsans(neighbor,adj,vis,res);
+  void check(vector<vector<int>>& adj, vector<int>& ans, int index, vector<bool>& vis){
+      if(vis[index]){
+          return;
+      }
+      vis[index]=true;
+      ans.push_back(index);
+      for(int v: adj[index]){
+          if(!vis[v]){
+            //   vis[v]=true;
+              check(adj,ans,v,vis);
           }
       }
   }
     vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
-        int v=adj.size();
-        vector<int> vis(v,0);
-        vector<int> res;
-        
-        dfsans(0,adj,vis,res);
-        return res;
-        
+        int n=adj.size();
+        vector<bool>vis(n,false);
+        //vis[0]=true;
+        vector<int>ans;
+        check(adj,ans,0,vis);
+        return ans;
     }
 };
